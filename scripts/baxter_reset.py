3992d3c94fa10d1db3b0710e81af9f131f719f73
Acarfi <carfi.alessandro@gmail.com>
2017-06-14 08:51:36 +0200

    new script for resetting baxter

---
+++ scripts/baxter_reset.py
@@ -0,0 +1,13 @@
+import os
+open_gripper = 'rostopic pub -1 /robot/end_effector/left_gripper/command baxter_core_msgs/EndEffectorCommand' \
+               ' \'{ id :  65538,  command : release, args : "{ }", sender : "foo", "sequence" : 1 }\''
+
+close_gripper = 'rostopic pub -1 /robot/end_effector/left_gripper/command baxter_core_msgs/EndEffectorCommand' \
+                ' \'{ id :  65538,  command : grip, args : "{ }", sender : "foo", "sequence" : 1 }\''
+
+untuck = 'rosrun custom_baxter_tools tuck_arms.py -u'
+
+print 'reaching untuck position'
+os.system('xterm -iconic -e ' + untuck)print 'opening gripper'
+
+os.system('xterm -iconic -e ' + open_gripper)
